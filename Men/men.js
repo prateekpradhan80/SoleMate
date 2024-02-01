8825f4bda12c7371683d5457cf0de61d7ed65acd
Prateek <prateekpradhan80@gmail.com>
2024-02-01 21:04:27 +0530

    second

---
+++ Men/men.js
@@ -0,0 +1,29 @@
+let itemsContainerElement = document.querySelector(".items-container");
+let innerHTML = "";
+items.map((item) => {
+  innerHTML += `<div class="
+  item-container">
+    <img class="item-image" src="${item.item_image}" alt="shoe image">
+    <div class="rating">
+      ${item.rating.stars} ⭐ | ${item.rating.noOfReviews}
+    </div>
+    <div class="company-name">${item.comapny_name}</div>
+    <div>
+      <label class="item-size" for="shoes">Choose your size</label>
+      <select name="Choose">
+        <option value="10"> US 10</option>
+        <option value="9"> US 9</option>
+         <option value="8"> US 8</option>
+         <option value="7">US 7</option>
+      </select>
+    </div>
+    <div class="price">
+       <span class="current-price">${item.current_price}</span>
+       <span class="original-price">${item.original_price}</span>
+       <span class="discount">${item.discount}</span>
+    </div>
+    <button class="btn-add-bag">Add to Bag</button>
+  </div>`;
+});
+
+itemsContainerElement.innerHTML = innerHTML;
